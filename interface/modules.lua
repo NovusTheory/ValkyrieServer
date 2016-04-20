924537cac2400f1921e4c37e2e545aec196eacc2
Gskartwii <gskw+git@crescentcode.net>
2016-04-20 09:56:32 -0400

    Convert modules.lua to PascalCase

--- interface/modules.lua
+++ interface/modules.lua
@@ -1,64 +1,63 @@
-local module                  = {};
-local modules                 = require "interface.modulespec";
-local encoder                 = library("encode");
-local auth                    = library("check_cokey");
-local inspect                 = require("inspect");
-local perms                   = library("permissions");
+local Module                  = {};
+local Modules                 = require "interface.modulespec";
+local Encoder                 = Library("encode");
+local Auth                    = Library("check_cokey");
+local Inspect                 = require("inspect");
+local Perms                   = Library("permissions");
 
-module                        = setmetatable(module, {
-  __index                     = function(self, module)
-    perms.parsePermissions();
-    local moduleMeta  = modules[module];
-    if moduleMeta == nil then
+Module                        = setmetatable(Module, {
+  __index                     = function(self, Module)
+    Perms.ParsePermissions();
+    local ModuleMeta  = Modules[Module];
+    if ModuleMeta == nil then
       error("Invalid module name!");
     end
 
-    local lib                 = library(moduleMeta.libName);
+    local Lib                 = Library(ModuleMeta.LibName);
 
     return setmetatable({}, {
-      __index                 = function(self2, funcName)
-        local functionMeta    = moduleMeta.functions[funcName];
-        if not functionMeta then
+      __index                 = function(self2, FuncName)
+        local FunctionMeta    = ModuleMeta.Functions[FuncName];
+        if not FunctionMeta then
           error("Invalid function name!");
         end
-        return function(request)
-          local passArgs      = {};
-          local missingArgs   = {};
-          local request       = request.params;
+        return function(Request)
+          local PassArgs      = {};
+          local MissingArgs   = {};
+          local Request       = Request.Params;
 
-          for i = 1, #functionMeta do
-            local metaType    = type(functionMeta[i]);
-            local requiredArg = metaType == "string" and functionMeta[i] or functionMeta[i].name;
+          for i = 1, #FunctionMeta do
+            local MetaType    = type(FunctionMeta[i]);
+            local RequiredArg = MetaType == "string" and FunctionMeta[i] or FunctionMeta[i].Name;
 
-            if request[requiredArg] ~= nil then
-              table.insert(passArgs, request[requiredArg]);
+            if Request[RequiredArg] ~= nil then
+              table.insert(PassArgs, Request[RequiredArg]);
             else
-              if metaType == "table" and functionMeta[i].norequire then
-                table.insert(passArgs, functionMeta[i].default);
+              if MetaType == "table" and FunctionMeta[i].NoRequire then
+                table.insert(PassArgs, FunctionMeta[i].Default);
               else
-                table.insert(missingArgs, requiredArg);
+                table.insert(MissingArgs, RequiredArg);
               end
             end -- if parsedbody
           end -- for
 
-          if #missingArgs ~= 0 then
-            error("Missing arguments: " .. table.concat(missingArgs, ", "));
+          if #MissingArgs ~= 0 then
+            error("Missing arguments: " .. table.concat(MissingArgs, ", "));
           end
 
-          if not perms.getPermission(request.gid, "modules.require") then
+          if not Perms.GetPermission(Request.GID, "modules.require") then
             return ({success = false; error = "You do not have the permission modules.require"});
-          elseif not perms.getPermission(request.gid, "modules.function") then
+          elseif not Perms.GetPermission(Request.GID, "modules.function") then
             return ({success = false; error = "You do not have the permission modules.function"});
-          elseif not perms.getPermission(request.gid, ("%s.%s"):format(module, funcName)) then
-            return ({success = false; error = "You do not have the permission " .. ("%s.%s"):format(module, funcName)});
+          elseif not Perms.GetPermission(Request.GID, ("%s.%s"):format(Module, FuncName)) then
+            return ({success = false; error = "You do not have the permission " .. ("%s.%s"):Format(Module, FuncName)});
           end
 
-          if not moduleMeta.skipAuth then
-            auth.check_nouid(request.gid, request.cokey);
+          if not ModuleMeta.SkipAuth then
+            Auth.CheckNoUID(Request.GID, Request.CoKey);
           end
 
-          --io.write(("\27[34;1m%s.%s{\27[36m%s\27[0m} - %s:%s\n"):format(module, funcName, table.concat(passArgs, ", "), request.gid, request.cokey));
-          return lib[funcName](unpack(passArgs));
+          return Lib[FuncName](unpack(PassArgs));
         end; -- return
       end; -- __index
     }); -- setmetatable
