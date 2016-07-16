b887d1df9a7dada6906e0c7d8f616f6d4e295caf
CrescentCode Admin / CI <gskw+ci@crescentcode.net>
2016-07-16 10:12:32 -0400

    Migrate bans.lua to the new database system

--- lib/bans.lua
+++ lib/bans.lua
@@ -1,10 +1,12 @@
 local Module      = {};
 local MySQL       = require "lapis.db";
-local Encoder     = library("encode");
+local Encoder     = Library("encode");
+local PlayerInfo  = Library("userinfo");
 
 local YieldError = require"lapis.application".yield_error;
 
 function Module.CreateBan(GID, Player, Reason)
+    local Player = PlayerInfo.RobloxToInternal(Player);
   local BanExists = mysql.select("id from bans where player=?", Player);
   if #BanExists > 0 then
     YieldError("That user is already banned!");
@@ -20,7 +22,7 @@ function Module.CreateBan(GID, Player, Reason)
 end
 
 function Module.IsBanned(Player)
-  local BanExists = mysql.select("* from bans where player=?", Player);
+  local BanExists = mysql.select("* from bans where player=?", PlayerInfo.RobloxToInternal(Player));
   if #BanExists > 0 then
     return ({success = true; error = ""; result = {true, BanExists[1].reason, BanExists[1].from_gid}});
   end
