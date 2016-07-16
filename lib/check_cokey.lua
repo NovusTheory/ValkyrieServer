da54a8d3ab65e02cba8b6ab58558ed702d233ea3
CrescentCode Admin / CI <gskw+ci@crescentcode.net>
2016-07-16 10:16:47 -0400

    Migrate check_cokey.lua to the new database system

--- lib/check_cokey.lua
+++ lib/check_cokey.lua
@@ -6,12 +6,12 @@ local AppHelpers = require"lapis.application";
 local YieldError = AppHelpers.yield_error;
 
 function Module.Check(GID, CoKey, UID)
-  local GameIDResult  = MySQL.select("id from game_ids where gid=? and cokey=md5(?)", GID, CoKey); -- Don't worry, Lapis will escape the strings
+  local GameIDResult  = MySQL.select("id from game_ids where gid=? and (uses_md5=1 and cokey=md5(?) or cokey=sha2(?, 256))", GID, CoKey, CoKey); -- Don't worry, Lapis will escape the strings
   if #GameIDResult < 1 then
     YieldError("Invalid UID-GID-CoKey combination!");
   end
 
-  local UserIDResult = MySQL.select("id from ? where uid=? and connection_key=md5(?)", gid_table("trusted_users", GID), UID, CoKey);
+  local UserIDResult = MySQL.select("id from trusted_users where uid=? and gid=? and (uses_md5=1 and connection_key=md5(?) or connection_key=sha2(?, 256))", UID, GID, CoKey, CoKey);
   if #UserIDResult < 1 then
     YieldError("Invalid UID-CoKey-GID combination!");
   end
@@ -20,7 +20,7 @@ function Module.Check(GID, CoKey, UID)
 end
 
 function Module.CheckNoUID(gid, cokey)
-  local GameIDResult  = MySQL.select("id from game_ids where gid=? and cokey=md5(?)", GID, CoKey);
+  local GameIDResult  = MySQL.select("id from game_ids where gid=? and (uses_md5=1 and cokey=md5(?) or cokey=sha2(?, 256))", GID, CoKey, CoKey);
   if #GameIDResult < 1 then
     YieldError("Invalid GID-CoKey pair!");
   end
