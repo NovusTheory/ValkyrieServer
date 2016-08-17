dcee2b88217b0a86316f8ebd91c6a0cd3d2f771e
NovusTheory <NovusTheory@users.noreply.github.com>
2016-08-17 14:14:57 -0500

    Modify backend of DELETE /game/:GID

--- app.lua
+++ app.lua
@@ -399,6 +399,9 @@ end);
 
 App:match("game", "/game/:GID", respond_to{
     GET = function(self)
+        if self.session.User then
+          self.CSRFToken = CSRF.generate_token(self, self.session.User)
+        end
         return {render = true};
     end,
     PUT = function(self)
@@ -481,9 +484,15 @@ App:match("game", "/game/:GID", respond_to{
           self.Invalid.GID = "Error: Invalid CSRF Token! (This message should never be displayed on a browser. If it has, contant gskw)";
         end
         
-        if MySQL.delete("game_ids", { gid = self.params.GID }) then
-          return "<script>window.location.replace('/user/" .. self.session.User .. "/games');</script>";
+        if MySQL.select("count(b.id) from users a left join game_ids b on b.owner = a.id where a.username = ? and b.gid = ?", self.session.User, self.params.GID)[1]["count(b.id)"] == "1" then
+            if MySQL.delete("game_ids", { gid = self.params.GID }) then
+              return {json = {success = true}};
+            end
+        else
+          self.Invalid.GID = "Error: User does not own GID"
         end
+        
+        return {json = {success = false, error = self.Invalid}}
     end
 });
 App:match("achievement", "/game/:GID/achievements/:Achievement", respond_to{
